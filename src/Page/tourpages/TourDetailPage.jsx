70032c4c37a8ac6e3a25f82f6dec4aacd62af608
TranVanThang510003 <113326669+TranVanThang510003@users.noreply.github.com>
2024-12-14 20:28:20 +0700

    update UI TourDetailPage

--- src/Page/tourpages/TourDetailPage.jsx
+++ src/Page/tourpages/TourDetailPage.jsx
@@ -1,7 +1,4 @@
-
-
-// export default TourDetailPage;
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState , useRef} from 'react';
 import { useParams, useNavigate } from 'react-router-dom'; // Import useNavigate
 import Footer from '../../layout/Footer';
 import ImageGallery from '../../components/common/ImageGallery';
@@ -9,50 +6,40 @@ import TourTitleAndRating from '../../components/common/TitleAndRating';
 import HighlightSection from '../../components/common/HighlightSection';
 import DetailSection from '../../components/common/DetailSection';
 import BookingCard from '../../components/common/BookingCard';
-import LikeButton from '../../components/common/LikeButton';
-import ShareButton from '../../components/common/ShareButton';
 import BookingForm from '../../components/Tour/TourDetail/BookingForm';
 import ReturnButton from '../../components/common/returnButton';
 import ReviewCard from '../../components/common/ReviewCard';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import UpdateTourForm from '../../components/services/updateService/updateTour/UpdateTourForm';
-import { fetchTourDetails } from '../../components/services/api';
-
-const reviews = [
-  {
-    avatar: "/img/avatar1.jpg",
-    username: "Nguyen **",
-    date: "27/2",
-    rating: 5,
-    title: "Rất hài lòng",
-    content:
-        "Lớp học rất thú vị, ấm cúng; nhân viên thân thiện, nhiệt tình và có thể giao tiếp đồng thời tiếng Anh và tiếng Việt...",
-    isLong: true,
-    images: [
-      "/img/review1.jpg",
-      "/img/review2.jpg",
-      "/img/review3.jpg",
-      "/img/review4.jpg",
-      "/img/review5.jpg",
-    ],
-  },
-  // Thêm các đánh giá khác tại đây
-];
-
+import { fetchTourDetails, fetchTourReviews } from '../../components/services/api'; // Gọi API từ backend
 
 const TourDetailPage = () => {
   const [tour, setTour] = useState(null);
+  const [reviews, setReviews] = useState([]); // State lưu danh sách đánh giá từ API
   const [loading, setLoading] = useState(true);
   const [isEditing, setIsEditing] = useState(false); // Trạng thái hiển thị form chỉnh sửa
   const { tourId } = useParams();
 
   const [userRole, setUserRole] = useState(
-      JSON.parse(localStorage.getItem("user"))?.role|| null
+      JSON.parse(localStorage.getItem("user"))?.role || null
   );
   const [userId, setUserId] = useState(() => {
     const user = localStorage.getItem('user');
     return user ? parseInt(JSON.parse(user).id, 10) : null;
   });
+
+  const bookingFormRef = useRef(null); // Tham chiếu đến Booking Form
+
+  // Hàm cuộn xuống Booking Form
+  const scrollToBookingForm = () => {
+    const offset = 100; // Thêm khoảng đệm để tránh bị che bởi header
+    const bookingFormPosition = bookingFormRef.current?.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+      top: bookingFormPosition - offset, // Cuộn đến vị trí trừ đi khoảng đệm
+      behavior: 'smooth', // Thêm hiệu ứng cuộn mượt
+    });
+  };
+
 
   useEffect(() => {
     const loadTourDetails = async () => {
@@ -67,7 +54,17 @@ const TourDetailPage = () => {
       }
     };
 
+    const loadTourReviews = async () => {
+      try {
+        const response = await fetchTourReviews(tourId); // Gọi API lấy đánh giá từ backend
+        setReviews(response.reviews || []);
+      } catch (error) {
+        console.error('Error fetching tour reviews:', error.message);
+      }
+    };
+
     loadTourDetails();
+    loadTourReviews();
   }, [tourId]);
 
   if (loading) return <p>Loading...</p>;
@@ -82,91 +79,103 @@ const TourDetailPage = () => {
       ?.filter(schedule => new Date(schedule.departureDate) >= today) // Lọc những lịch trình trong tương lai
       .sort((a, b) => new Date(a.departureDate) - new Date(b.departureDate))[0]; // Sắp xếp và lấy lịch trình gần nhất
 
-// Nếu không có lịch trình trong tương lai, tìm lịch trình có ngày khởi hành gần nhất trong quá khứ
+  // Nếu không có lịch trình trong tương lai, tìm lịch trình có ngày khởi hành gần nhất trong quá khứ
   const finalSchedule = nearestSchedule || tour.schedules
       ?.filter(schedule => new Date(schedule.departureDate) < today) // Lọc những lịch trình trong quá khứ
       .sort((a, b) => new Date(b.departureDate) - new Date(a.departureDate))[0]; // Sắp xếp và lấy lịch trình gần nhất trong quá khứ
 
-// Lấy giá người lớn từ lịch trình cuối cùng (tương lai nếu có, quá khứ nếu không)
+  // Lấy giá người lớn từ lịch trình cuối cùng (tương lai nếu có, quá khứ nếu không)
   const priceAdult = finalSchedule?.priceAdult || tour.priceAdult || 0; // Nếu không có lịch trình gần nhất, lấy giá mặc định từ tour
 
-
   return (
-    <div className="bg-[#F8F8F8]">
-      <div className="w-4/5 mx-auto mt-[80px]">
+      <div className="bg-[#F8F8F8]">
+        <div className="w-4/5 mx-auto mt-[80px]">
 
-        {/* Kiểm tra trạng thái isEditing để hiển thị giao diện tương ứng */}
-        {isEditing ? (
-          // Hiển thị form chỉnh sửa
-          <UpdateTourForm
-            tourData={tour} // Truyền dữ liệu tour vào form chỉnh sửa
-            onCancel={() => setIsEditing(false)} // Hàm để hủy chỉnh sửa
-          />
-        ) : (
-          // Hiển thị chi tiết tour
-          <>
-            <div className="my-3">
-              <ReturnButton />
-            </div>
-            <ImageGallery
-              images={tour.images?.map((img) => img.imageUrl) || []}
-            />
-
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-              <div className="md:col-span-2">
-                <TourTitleAndRating
-                    title={tour.name}
-                    rating={tour.averageRating}
-                    reviewsCount={tour.reviewCount}
-                    location={tour.location}
-                    serviceType={tour.serviceType}
-                    language={tour.language}
-                    tourType={tour.tourType}
-                />
-                <HighlightSection highlights={tour.highlights}/>
-                <DetailSection details={tour.services}/>
-                <BookingForm
-                    priceAdult={tour.priceAdult}
-                    priceChild={tour.priceChild}
-                    tour_id={tour.id}
-                    schedules={tour.schedules}
-
-                />
-              </div>
-
-              <div className="md:col-span-1">
-                <BookingCard
-                    price={priceAdult}
-                    onBookingClick={() => console.log('Đã nhấn đặt tour!')}
-                />
-                <div className="flex justify-between mt-2">
-                  <LikeButton/>
-                  <ShareButton/>
+          {isEditing ? (
+              // Hiển thị form chỉnh sửa
+              <UpdateTourForm
+                  tourData={tour}
+                  onCancel={() => setIsEditing(false)} // Hàm để hủy chỉnh sửa
+              />
+          ) : (
+              // Hiển thị chi tiết tour
+              <>
+                <div className="my-3">
+                  <ReturnButton />
                 </div>
-              </div>
-              <div className="p-4 col-span-2">
-                <h1 className="text-3xl font-bold mb-2">Đánh giá</h1>
+                <ImageGallery
+                    images={tour.images?.map((img) => img.imageUrl) || []}
+                />
 
-                {reviews.map((review, index) => (
-                    <ReviewCard key={index} review={review}/>
-                ))}
-              </div>
-            </div>
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                  <div className="md:col-span-3">
+                    <TourTitleAndRating
+                        title={tour.name}
+                        rating={tour.averageRating}
+                        reviewsCount={tour.reviewCount}
+                        location={tour.location}
+                        serviceType={tour.serviceType}
+                        language={tour.language}
+                        tourType={tour.tourType}
+                    />
+                    <HighlightSection highlights={tour.highlights}/>
+                    <DetailSection details={tour.services}/>
+                    <div ref={bookingFormRef}>
+                      <BookingForm
+                          priceAdult={priceAdult}
+                          priceChild={tour.priceChild}
+                          tour_id={tour.id}
+                          schedules={tour.schedules}
+                      />
+                    </div>
+                  </div>
 
-            {canEdit && (
-                <button
-                    className="text-6xl fixed bottom-20 right-20 text-blue-500 bg-white rounded-full shadow-lg p-4 hover:bg-blue-700 hover:text-white transition"
-                onClick={() => setIsEditing(true)} // Kích hoạt trạng thái chỉnh sửa
-              >
-                <Icon icon="game-icons:feather" />
-              </button>
-            )}
-          </>
-        )}
+                  {/* Booking Card */}
+                  <div className="md:col-span-1">
+                    <div className="sticky top-28">
+                      <BookingCard price={priceAdult} onBookingClick={scrollToBookingForm}/>
+                    </div>
+                  </div>
+                  <div className="p-4 col-span-3">
+                    <h1 className="text-3xl font-bold mb-2 flex items-center gap-2">
+                      Đánh giá
+                      <span className="flex items-center gap-1 text-yellow-500">
+      {/* Hiển thị sao trung bình */}
+                        {[...Array(5)].map((_, index) => (
+                            <span
+                                key={index}
+                                className={index < Math.round(tour.averageRating) ? "text-yellow-500" : "text-gray-300"}
+                            >
+          ★
+        </span>
+                        ))}
+    </span>
+                      {/* Hiển thị số lượt đánh giá */}
+                      <span className="text-gray-600 text-lg">({tour.reviewCount || 0} lượt đánh giá)</span>
+                    </h1>
 
+                    {reviews.map((review, index) => (
+                        <ReviewCard key={index} review={review}/>
+                    ))}
+                  </div>
+
+
+                </div>
+
+                {canEdit && (
+                    <button
+                        className="text-6xl fixed bottom-20 right-20 text-blue-500 bg-white rounded-full shadow-lg p-4 hover:bg-blue-700 hover:text-white transition"
+                        onClick={() => setIsEditing(true)} // Kích hoạt trạng thái chỉnh sửa
+                    >
+                      <Icon icon="game-icons:feather"/>
+                    </button>
+                )}
+              </>
+          )}
+
+        </div>
+        <Footer/>
       </div>
-        <Footer />
-    </div>
   );
 };
 
